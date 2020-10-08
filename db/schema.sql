d3f29449b8112fb2743b77fec0934b60a0855ee5
durganp1 <durganp1@gmail.com>
2020-10-08 15:39:55 -0500

    set up connection, server and schema

---
+++ db/schema.sql
@@ -0,0 +1,5 @@
+
+
+DROP DATABASE IF EXISTS petro_tech_blog_db;
+
+CREATE DATABASE petro_tech_blog_db;
---
+++ server.js
@@ -0,0 +1,39 @@
+
+
+const express = require('express');
+const routes = require('./controllers');
+const sequelize = require('./config/connections');
+const path = require('path');
+
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+require ('dotenv').config();
+const sess = {
+    secret: process.env.SESS_SECRET,
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
+
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({helpers});
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+});

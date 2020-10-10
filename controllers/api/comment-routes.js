55d3af3a839701c6c5909642789c68da67c25ffa
durganp1 <durganp1@gmail.com>
2020-10-09 19:24:45 -0500

    added homepage route

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -16,7 +16,6 @@ router.get('/', (req, res) => {
 });
 
 router.post('/', withAuth, (req, res) => {
-    console.log('+++++++++++++',req.body);
     Comment.create({
         comment_text: req.body.comment_text,
         post_id: req.body.post_id,
